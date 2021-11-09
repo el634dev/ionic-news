75c767704c5af3ca46b2b29f08fc1729265f8c30
el634dev <84924260+el634dev@users.noreply.github.com>
2021-11-09 15:52:20 -0500

    Add files via upload

---
+++ src/app/about/about.page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-about',
+    templateUrl: 'about.page.html',
+    styleUrls: ['about.page.scss']
+})
+
+export class AboutPage {}
